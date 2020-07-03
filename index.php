6c01e5da87f5e118ef0d7c5e628e322b9c54fa77
Manohisoa Randriambahiny <randriambahinymanohy@gmail.com>
2020-07-03 12:55:54 +0300

    first api

--- index.php
+++ index.php
@@ -20,13 +20,13 @@ require 'routes/routeNgam.php';
 require 'routes/routeBio.php';
 
 
-// $dbopts = parse_url(getenv('DATABASE_URL'));
-// Flight::register('db', 'Connect', array($dbopts["host"], $dbopts["port"], ltrim($dbopts["path"], '/'), $dbopts["user"], $dbopts["pass"]));
+$dbopts = parse_url(getenv('DATABASE_URL'));
+Flight::register('db', 'Connect', array($dbopts["host"], $dbopts["port"], ltrim($dbopts["path"], '/'), $dbopts["user"], $dbopts["pass"]));
 
 // Flight::register('db', 'Connect', array('localhost','5432','volako_db','volako_user','1234'));
-Flight::register('db', 'PDO', array('pgsql:host=localhost;port=5432;dbname=safecorner_db', 'postgres', 'm1234'), function ($db) {
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-});
+// Flight::register('db', 'PDO', array('pgsql:host=localhost;port=5432;dbname=safecorner_db', 'postgres', 'm1234'), function ($db) {
+//   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// });
 
 
 Flight::start();
