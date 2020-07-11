ca4329258695c38c4971555a35d6b01b22a05a98
Manohisoa Randriambahiny <randriambahinymanohy@gmail.com>
2020-07-11 21:06:56 +0300

    .

--- outil/auth.php
+++ outil/auth.php
@@ -292,13 +292,13 @@ Flight::map('getTokenHeader', function (string $type) {
 //Content-Type,Connection,Accept
 Flight::map('getAccesControl', function () {
         header('Access-Control-Allow-Origin: https://safe-corner-api.herokuapp.com');
-        header('Access-Control-Allow-Headers: sc-access-token,sc-mobile-token,sc-init,sc-refresh-token');
+        header('Access-Control-Allow-Headers: sc-access-token,sc-refresh-token');
         header('Content-Type: application/json; charset=utf-8');
         header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,OPTIONS');
 });
 
 Flight::map('getAccesControlPublic', function () {
-    header('Access-Control-Allow-Headers:sc-mobile-token');
+    header('Access-Control-Allow-Headers:sc-mobile-token,sc-init');
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=utf-8');
     header('Access-Control-Allow-Methods: GET,OPTIONS');
